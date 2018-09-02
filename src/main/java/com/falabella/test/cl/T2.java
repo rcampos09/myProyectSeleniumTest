25b35a43cddfc4c63745208d01e7bd5cb4a8db83
Rodrigo Antonio Campos Tapia <rcampos@MacBook-Pro-de-Rodrigo.local>
2018-09-02 20:29:06 -0300

    se realiza cambios en source testng y maven

--- src/main/java/com/falabella/test/cl/T2.java
+++ src/main/java/com/falabella/test/cl/T2.java
@@ -1,17 +1,21 @@
 package com.falabella.test.cl;
 
 import org.testng.Assert;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
+import com.dataprovider.IntegratedDataProvider;
+import com.entities.Product;
 import com.falabella.pages.cl.HomePage;
 import com.falabella.pages.cl.PDPPage;
 import com.seleniumTest.BaseConfig;
 
 public class T2 extends BaseConfig {
   
-  @Test()
-  private void Searh_Product_In_HomePage_2() {
+  @Test(dataProvider = "Product2Falabella", dataProviderClass = IntegratedDataProvider.class)
+  @Parameters({ "browserRun" })
+  private void Searh_Product_In_HomePage_2(Product product) {
     HomePage homePage = new HomePage(driver);
-    homePage.SearhSku("1348765");
+    homePage.SearhSku(product.Sku());
     PDPPage pdpPage = new PDPPage(driver);
     Assert.assertTrue(pdpPage.valid_PDP_Product(),"Div Product is not display");
   }
