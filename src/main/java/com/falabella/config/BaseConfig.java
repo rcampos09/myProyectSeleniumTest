ae0ee33e95b52ea15fbbf882c751f7f800aea18a
Rodrigo Antonio Campos Tapia <rcampos@MacBook-Pro-de-Rodrigo-2.local>
2018-09-15 16:03:37 -0300

    new changes for Screenshot

--- src/main/java/com/falabella/config/BaseConfig.java
+++ src/main/java/com/falabella/config/BaseConfig.java
@@ -1,9 +1,15 @@
 package com.falabella.config;
 
+import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URI;
+import javax.swing.text.Utilities;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
@@ -22,7 +28,6 @@ public class BaseConfig {
       browser.setBrowserName("chrome");
       browser.setVersion("66.0");
       browser.setCapability("enableVNC", true);
-
       try {
         driver = new RemoteWebDriver(URI.create(urlSelenoid + portSelenoid).toURL(), browser);
         driver.manage().window().fullscreen();
@@ -36,7 +41,43 @@ public class BaseConfig {
   }
 
   @AfterMethod
-  public void closeDriver() {
+  public void closeDriver(ITestResult result) {
+    if (ITestResult.FAILURE == result.getStatus()) {
+      try {
+        // Create refernce of TakesScreenshot
+        TakesScreenshot ts = (TakesScreenshot) driver;
+        // Call method to capture screenshot
+        File source = ts.getScreenshotAs(OutputType.FILE);
+        FileUtils.copyFile(source, new File("./Screenshots/" + result.getName() + ".png"));
+        System.out.println("Screenshot taken");
+      } catch (Exception e) {
+        System.out.println("Exception while taking screenshot " + e.getMessage());
+      }
+    }
+    if (ITestResult.SUCCESS == result.getStatus()) {
+      try {
+        // Create refernce of TakesScreenshot
+        TakesScreenshot ts = (TakesScreenshot) driver;
+        // Call method to capture screenshot
+        File source = ts.getScreenshotAs(OutputType.FILE);
+        FileUtils.copyFile(source, new File("./Screenshots/" + result.getName() + ".png"));
+        System.out.println("Screenshot taken");
+      } catch (Exception e) {
+        System.out.println("Exception while taking screenshot " + e.getMessage());
+      }
+    }
+    if (ITestResult.SKIP == result.getStatus()) {
+      try {
+        // Create refernce of TakesScreenshot
+        TakesScreenshot ts = (TakesScreenshot) driver;
+        // Call method to capture screenshot
+        File source = ts.getScreenshotAs(OutputType.FILE);
+        FileUtils.copyFile(source, new File("./Screenshots/" + result.getName() + ".png"));
+        System.out.println("Screenshot taken");
+      } catch (Exception e) {
+        System.out.println("Exception while taking screenshot " + e.getMessage());
+      }
+    }
     driver.quit();
   }
 }
--- src/main/java/com/falabella/test/cl/T2.java
+++ src/main/java/com/falabella/test/cl/T2.java
@@ -7,11 +7,13 @@ import com.falabella.dataprovider.IntegratedDataProvider;
 import com.falabella.entities.Product;
 import com.falabella.pages.cl.HomePage;
 import com.falabella.pages.cl.PDPPage;
+import io.qameta.allure.Description;
 
 public class T2 extends BaseConfig {
   
   @Test(dataProvider = "Product2Falabella", dataProviderClass = IntegratedDataProvider.class)
-  private void Searh_Product_In_HomePage_2(Product product) {
+  @Description("Searh Product in Home")
+  private void SearhProductInHomePage2(Product product) {
     HomePage homePage = new HomePage(driver);
     homePage.SearhSku(product.Sku());
     PDPPage pdpPage = new PDPPage(driver);
