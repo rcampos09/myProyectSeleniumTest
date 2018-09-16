893fa0460d8ce9c586c316850c08e8a26875b901
Rodrigo Antonio Campos Tapia <rcampos@MacBook-Pro-de-Rodrigo-2.local>
2018-09-16 19:46:28 -0300

    new changes for anotation allure

--- src/main/java/com/falabella/test/cl/T2.java
+++ src/main/java/com/falabella/test/cl/T2.java
@@ -1,6 +1,7 @@
 package com.falabella.test.cl;
 
 import org.testng.Assert;
+import org.testng.IHookable;
 import org.testng.annotations.Test;
 import com.falabella.config.BaseConfig;
 import com.falabella.dataprovider.IntegratedDataProvider;
@@ -8,10 +9,14 @@ import com.falabella.entities.Product;
 import com.falabella.pages.cl.HomePage;
 import com.falabella.pages.cl.PDPPage;
 import io.qameta.allure.Description;
+import io.qameta.allure.Step;
+import io.qameta.allure.Story;
 
-public class T2 extends BaseConfig {
+public class T2 extends BaseConfig implements IHookable {
   
   @Test(dataProvider = "Product2Falabella", dataProviderClass = IntegratedDataProvider.class)
+  @Story("Sprint 20")
+  @Step("Type {product.Sku()}")
   @Description("Searh Product in Home")
   private void SearhProductInHomePage2(Product product) {
     HomePage homePage = new HomePage(driver);
