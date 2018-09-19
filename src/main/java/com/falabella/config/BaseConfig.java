0b13f43a1f8bd97df5208e2d8e81768fbf9362f8
Rodrigo Antonio Campos Tapia <rcampos@MacBook-Pro-de-Rodrigo-2.local>
2018-09-19 00:13:38 -0300

    new changes for Listener

--- src/main/java/com/falabella/config/BaseConfig.java
+++ src/main/java/com/falabella/config/BaseConfig.java
@@ -1,14 +1,9 @@
 package com.falabella.config;
 
-import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URI;
-import org.apache.commons.io.FileUtils;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
-import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
@@ -38,45 +33,9 @@ public class BaseConfig {
     }
     return driver;
   }
-  
+
   @AfterMethod
-  public void closeDriver(ITestResult result) {
-    if (ITestResult.FAILURE == result.getStatus()) {
-      try {
-        // Create reference of TakesScreenshot
-        TakesScreenshot ts = (TakesScreenshot) driver;
-        // Call method to capture screenshot
-        File source = ts.getScreenshotAs(OutputType.FILE);
-        FileUtils.copyFile(source, new File("./Screenshots/" + result.getName() + ".png"));
-        System.out.println("Screenshot taken");
-      } catch (Exception e) {
-        System.out.println("Exception while taking screenshot " + e.getMessage());
-      }
-    }
-    if (ITestResult.SUCCESS == result.getStatus()) {
-      try {
-        // Create reference of TakesScreenshot
-        TakesScreenshot ts = (TakesScreenshot) driver;
-        // Call method to capture screenshot
-        File source = ts.getScreenshotAs(OutputType.FILE);
-        FileUtils.copyFile(source, new File("./Screenshots/" + result.getName() + ".png"));
-        System.out.println("Screenshot taken");
-      } catch (Exception e) {
-        System.out.println("Exception while taking screenshot " + e.getMessage());
-      }
-    }
-    if (ITestResult.SKIP == result.getStatus()) {
-      try {
-        // Create reference of TakesScreenshot
-        TakesScreenshot ts = (TakesScreenshot) driver;
-        // Call method to capture screenshot
-        File source = ts.getScreenshotAs(OutputType.FILE);
-        FileUtils.copyFile(source, new File("./Screenshots/" + result.getName() + ".png"));
-        System.out.println("Screenshot taken");
-      } catch (Exception e) {
-        System.out.println("Exception while taking screenshot " + e.getMessage());
-      }
-    }
+  public void closeDriver() {
     driver.quit();
   }
 }
